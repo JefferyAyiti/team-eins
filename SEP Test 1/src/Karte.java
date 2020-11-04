b674cb7897a12be1e9cbe6c87eb46c32cb08506d
mfauth@rhrk.uni-kl.de <7yM@H2LQ>
2020-11-04 11:50:07 +0100

    git-test 2

--- SEP Test 1/src/Karte.java
+++ SEP Test 1/src/Karte.java
@@ -1,2 +1,17 @@
 public class Karte {
+    int wert;
+    String farbe;
+
+    public Karte(int wert, String farbe) {
+        this.wert = wert;
+        this.farbe = farbe;
+    }
+
+    public String getFarbe() {
+        return farbe;
+    }
+
+    public int getWert() {
+        return wert;
+    }
 }
