d14169c8f22389d26a406352f599faeb168a5460
Sebastian Hesse <Sebastian.hesse99@web.de>
2020-11-17 20:31:31 +0100

    Documentation

--- Team Eins/src/HandKarte.java
+++ Team Eins/src/HandKarte.java
@@ -3,12 +3,22 @@ public class HandKarte extends Karte {
     private Tisch tisch;
     private Hand hand;
 
+    /**
+     * @param value
+     * @param covered
+     * @param entryTisch
+     * @param entryHand
+     */
     public HandKarte(int value, boolean covered, Tisch entryTisch, Hand entryHand) {
         super(value, covered);
         tisch = entryTisch;
         hand = entryHand;
     }
 
+    /**
+     * legt die Karte auf den Tisch und entfernt die Karte aus der Hand
+     * @throws Exception
+     */
     void ablegen() throws Exception {
         if(this.isPlayable()) {
             tisch.karteAblegen(this);
@@ -16,6 +26,10 @@ public class HandKarte extends Karte {
         }
     }
 
+    /**
+     * @return Ob die aktuelle Karte ablegbar ist oder nicht
+     * @throws Exception
+     */
     boolean isPlayable() throws Exception {
         //3 Ablageregeln
         boolean ruleNormal = value != LAMA && (
