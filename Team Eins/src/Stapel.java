320c9176ea278d8b49543b7b713fc514cf180dc8
Dhruvil <pateld@rhrk.uni-kl.de>
2020-11-17 16:08:39 +0100

    Stapel JavaDoc

--- Team Eins/src/Stapel.java
+++ Team Eins/src/Stapel.java
@@ -3,16 +3,41 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Stack;
 
+/**
+ * Stapeldatenstruktur für den Zieh und -Ablagestapel.
+ */
 public class Stapel {
+
+    /**
+     * cardCount steht für die Anzahl der Karten im Stapel
+     */
     private int cardCount = 0;
+    /**
+     * coverCard steht für den Typ des Stapels.
+     * True für Ziehstapel. False für Ablagestapel
+     */
     private boolean coverCard;
-    private Stack<Karte> stapel = new Stack<Karte>();
+    /**
+     * stapel steht für den Stapel dargestellt als stack
+     */
+    private Stack<Karte> stapel = new Stack<>();
+    /**
+     * topCard ist die oberste Karte des Stapels
+     */
     private Karte topCard;
 
+    /** Der Stapel wird mit dem Typ des Stapels initialisiert
+     * @param coverCard True für Ziehstapel. False für Ablagestapel
+     */
     public Stapel(boolean coverCard) {
         this.coverCard = coverCard;
     }
 
+    /**Ziehe die oberste Karte aus dem Ziehstapel.
+     *  Vorher muss geguckt werden, ob es sich um den Ziehstapel handelt.
+     * @return Karte die gezogen wurde
+     * @throws Exception
+     */
     public Karte ziehen() throws Exception {
         if(stapel.isEmpty()){
             throw new Exception("Stapel leer");
@@ -27,16 +52,25 @@ public class Stapel {
         throw new Exception("Stapel leer");
     }
 
-    public void ablegen(Karte karte){
+
+    /**Falls es sich um den Ablagestapel handelt, lege die Karte auf dem Ablagestapel.
+     * @param karte Diese Karte wird auf dem Ablagestapel gelegt
+     * @throws Exception
+     */
+    public void ablegen(Karte karte) throws Exception {
         if (!coverCard){    //Ablagestapel
             stapel.add(karte);
             cardCount = cardCount + 1;
             topCard = stapel.peek();
         }
+        throw new Exception();
     }
 
+    /**Mische den Kartenstapel
+     *
+     */
     public void mischen(){
-        ArrayList<Karte> kartenStapel = new ArrayList<Karte>();
+        ArrayList<Karte> kartenStapel = new ArrayList<>();
 
         while(!stapel.isEmpty()){           //stapel leeren und in liste packen
             Karte karte = stapel.pop();
@@ -49,14 +83,25 @@ public class Stapel {
             stapel.push(kartenStapel.get(i));       //wieder auf stack packen
         }
     }
+
+    /**
+     * Kartenstapel leeren
+     */
     public void Reset(){
-         stapel = new Stack<Karte>();
+         stapel = new Stack<>();
     }
 
+    /** getter-Methode für cardCount
+     * @return cardCount
+     */
     public int getCardCount() {
         return cardCount;
     }
-    
+
+    /** getter-Methode für coverCard
+     * @return coverCard
+     * @throws Exception
+     */
     public Karte getTopCard() throws Exception {
         if(!coverCard){
             return topCard;
@@ -64,6 +109,10 @@ public class Stapel {
         throw new Exception();
     }
 
+    /**Füge zu dem Ziehstapel die Karte hinzu.
+     * @param karte Diese Karte wird zum Ziehstapel hinzugefügt
+     * @throws Exception
+     */
     public void addCard(Karte karte) throws Exception {
         if(coverCard){
             stapel.push(karte);
