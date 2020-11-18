75db67ed17a9e4bfff6a1a39ef89a1068141056b
mfauth@rhrk.uni-kl.de <7yM@H2LQ>
2020-11-18 15:30:56 +0100

    Spiellogik

---
+++ Team Eins/src/Spiellogik.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Spiellogik {
+    Stack<Spieler> letzteSpieler;
+
+
+    /**
+     * @param spieler
+     * @param karte
+     */
+    void karteLegen(Spieler spieler, Karte karte) {
+
+    }
+
+    void karteNachziehen(Spieler spieler) {
+
+    }
+
+    void chipsKassieren(Spieler spieler) {
+
+    }
+
+    void chipsTauschen(Spieler spieler) {
+
+    }
+
+    void chipAbgeben(Spieler spieler, Chip chip) {
+
+    }
+
+    void aussteigen(Spieler spieler) {
+
+    }
+
+    Map<Spieler, Integer> ranglisteErstellen() {
+        return new HashMap<>();
+    }
+
+}
+
