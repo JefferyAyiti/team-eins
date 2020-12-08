ca8008c5b5b5aa38f190fcb26a0651d597296228
Sebastian Hesse <Sebastian.hesse99@web.de>
2020-12-08 17:14:50 +0100

    RMI: Client Methoden hinzugefügt und Dokumentiert

--- Team Eins/src/RMI/RMIClient.java
+++ Team Eins/src/RMI/RMIClient.java
@@ -1,5 +1,7 @@
 package RMI;
 
+import Main.Tisch;
+
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -7,10 +9,37 @@ import java.rmi.registry.Registry;
 
 public class RMIClient {
     private final server server;
+    private Tisch tisch;
 
+    /**
+     * @param IP Server IP
+     * @param Port Server Port
+     * @param Server_Name
+     * @throws RemoteException
+     * @throws NotBoundException
+     */
     public RMIClient(String IP, int Port, String Server_Name) throws RemoteException, NotBoundException {
 
         Registry registry = LocateRegistry.getRegistry(IP, Port);
         server = (server) registry.lookup(Server_Name);
+    }
+
+    /**
+     * tisch Attribut wird durch das neue Attribut vom Server ersetzt
+     */
+    public void update() {
+        tisch = server.update();
+        //TODO Gui.update() (?)
+    }
+
+    /**
+     * Startet RunClient. Dieser Thread aktualisiert das Attribut tisch in regelmäßigen abständen.
+     */
+    public void startClientThread(){
+        ClientThread ct = new ClientThread((ServerImpl) server, this);
+        ct.run();
+    }
+    public Tisch getTisch(){
+        return tisch;
     }
 }
