41dcc9f5652c1e680e36e496b46d1a0ea45918de
Jeffery Ayiti <j_ayiti18@informatik.uni-kl.de>
2021-01-05 16:18:22 +0100

    Chat Optik 3

--- Team Eins/src/GUI/GUIChat.java
+++ Team Eins/src/GUI/GUIChat.java
@@ -5,6 +5,9 @@ import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Popup;
 import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
@@ -19,7 +22,7 @@ public class GUIChat {
     Popup chat;
     public TextField input = new TextField();
     private ScrollPane scroll = new ScrollPane();
-    public TextArea messages = new TextArea();
+    public  TextFlow messages = new TextFlow();
     private Button sendButton = new Button("Send");
 
 
@@ -35,17 +38,20 @@ public class GUIChat {
         chat.setAnchorX(X+5);
         chat.setAnchorY(Y);
         chat.setAutoHide(true);
-        scroll.setOpacity(0.7);
+        scroll.setOpacity(0.9);
         scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+
 
         VBox content = new VBox();
         content.setMaxHeight(300);
-        messages.setEditable(false);
-        messages.setWrapText(true);
+        messages.setPrefHeight(width+50);
         messages.setPrefWidth(width+70);
         messages.setMaxWidth(600);
+        //messages.setStyle("-fx-background-color:black;");
+        messages.setStyle("-fx-background-image: url('/GUI/images/oberflaeche.jpg');");
         HBox inputBox = new HBox();
-        input.setPrefWidth(width+27);
+        input.setPrefWidth(width+28);
         inputBox.getChildren().addAll(input, sendButton);
         onEnter();
         onPress();
@@ -61,24 +67,33 @@ public class GUIChat {
                 keyEvent -> {
                     if(keyEvent.getCode() == KeyCode.ENTER){
                         chatrecord.add(input.getText());
-                        messages.appendText(input.getText() + "\n");
+                        Text newMsg = new Text(input.getText() + "\n");
+                        newMsg.setFill(Color.WHITE);
+                        messages.getChildren().add(newMsg);
+                        //messages.setStyle("-fx-background-color:black;");
+                        messages.setStyle("-fx-background-image: url('/GUI/images/oberflaeche.jpg');");
+                        //to change
                         /*try {
                             server.updateClients(input.getText());
                         } catch (RemoteException e) {
-
                         }*/
                         input.clear();
                     } });
     }
 
     public void addText(String msg){
-        messages.appendText(msg + "\n");
-    }
+        messages.getChildren().add(new Text(msg + "\n"));
+    } // to change
 
     public void onPress(){
         sendButton.setOnMouseClicked( mouseEvent -> {
             chatrecord.add(input.getText());
-            messages.appendText(input.getText() + "\n");
+            //messages.getChildren().add(new Text(input.getText() + "\n")); //to change
+            Text newMsg = new Text(input.getText() + "\n");
+            newMsg.setFill(Color.WHITE);
+            messages.getChildren().add(newMsg);
+            messages.setStyle("-fx-background-image: url('/GUI/images/oberflaeche.jpg');");
+            //messages.setStyle("-fx-background-color:black;");
             /*try {
                 server.updateClients(input.getText());
             } catch (RemoteException e) {
